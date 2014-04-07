1a8c7ad986df264afc8fb3afdb19e0143c6ae20c
Simon Pedersen <mail@sjkp.dk>
2014-04-07 22:47:07 +0200

    allow cors

--- ProffScraper/server.js
+++ ProffScraper/server.js
@@ -2,6 +2,25 @@ var express = require('express');
 var proffdkscraper = require('./proffdkscraper.js');
 var port = process.env.port || 1337;
 var app = express();
+app.use(express.methodOverride());
+
+// ## CORS middleware
+// see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+
 
 app.get('/cvr/:id', function(req, res)
     {
